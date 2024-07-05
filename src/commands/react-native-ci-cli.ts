0828d558704119b5d0d97d3e1884db3a19701f78
Maciej Stosio <maciekstosio@users.noreply.github.com>
2024-07-05 11:52:00 +0200

    chore: add changes from comments

--- src/commands/react-native-ci-cli.ts
+++ src/commands/react-native-ci-cli.ts
@@ -1,23 +1,31 @@
 import { GluegunCommand } from 'gluegun'
 import runLint from '../recipies/lint'
-import { intro, outro } from '@clack/prompts'
+import { intro, outro, confirm } from '@clack/prompts'
 import { SKIP_INTERACTIVE_COMMAND } from '../constants'
+import isGitDirty from 'is-git-dirty'
 
 const command: GluegunCommand = {
   name: 'react-native-ci-cli',
   run: async (toolbox) => {
     intro('Welcome to React Native CI CLI')
 
-    if (toolbox.isGitDirty() == null) {
+    if (isGitDirty() == null) {
       outro('This is not a git repository. Exiting.')
 
       return
     }
 
-    if (toolbox.isGitDirty() == true) {
-      outro('Please commit your changes before running this command. Exiting.')
+    if (isGitDirty() == true) {
+      const proceed = await confirm({
+        message: 'You have uncommitted changes. Do you want to proceed?',
+      })
 
-      return
+      if (!proceed) {
+        outro(
+          'Please commit your changes before running this command. Exiting.'
+        )
+        return
+      }
     }
 
     const lintExecutor = await runLint(toolbox)
@@ -34,11 +42,14 @@ const command: GluegunCommand = {
     const executorResults = await Promise.all(
       executors.map((executor) => executor(toolbox))
     )
-    const usedFlags = (await executorResults).join(' --')
+
+    const usedFlags = executorResults.join(' --')
 
     toolbox.print.success(
       `We're all set. Next time you can use silent command: npx create-react-native-ci-cli --${SKIP_INTERACTIVE_COMMAND} ${usedFlags}`
     )
+
+    process.exit()
   },
 }
 
