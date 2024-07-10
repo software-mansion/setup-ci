25d1a0238c88e6c781741415b36d28ec59832aa3
Katzper Michno <katzper.michno@gmail.com>
2024-07-10 16:23:05 +0200

    fix: better installed dependencies detection (#10)
    * fix: better installed dependencies detection
    
    * split dependencies.exists into two functions

--- src/extensions/dependencies.ts
+++ src/extensions/dependencies.ts
@@ -3,26 +3,45 @@ import { GluegunToolbox } from 'gluegun'
 module.exports = (toolbox: GluegunToolbox) => {
   const { filesystem, packageManager, print } = toolbox
 
-  const exists = (name: string, dev: boolean) => {
+  const exists = (name: string): boolean => {
     const packageJSON = filesystem.read('package.json', 'json')
-    return (
-      (!dev && packageJSON?.dependencies?.[name]) ||
-      (dev && packageJSON?.devDependencies?.[name])
-    )
+    return !!packageJSON?.dependencies?.[name]
+  }
+
+  const existsDev = (name: string): boolean => {
+    const packageJSON = filesystem.read('package.json', 'json')
+    return !!packageJSON?.devDependencies?.[name]
   }
 
   const add = async (name: string, dev = false) => {
-    if (exists(name, dev)) {
+    if ((dev && existsDev(name)) || (!dev && exists(name))) {
       print.info(`${name} already installed, skipping adding dependency.`)
       return
+    }
+
+    if (dev && exists(name)) {
+      toolbox.print.warning(
+        `${name} is already in "dependencies", but shouldn't it be in "devDependencies"?`
+      )
+      return
+    }
+
+    if (!dev && existsDev(name)) {
+      toolbox.print.warning(
+        `Moving ${name} from "devDependencies" to "dependencies".`
+      )
+
+      const spinner = print.spin(
+        `🗑️ Removing ${name} from "devDependencies"...`
+      )
+      await packageManager.remove(name, { dev: true })
+      spinner.stop()
     }
 
     const spinner = print.spin(
       `📦 Installing ${name} as ${dev ? 'devDependency' : 'dependency'}...`
     )
-
     await packageManager.add(name, { dev })
-
     spinner.stop()
 
     print.info(`✔ Installed ${name}.`)
@@ -30,6 +49,7 @@ module.exports = (toolbox: GluegunToolbox) => {
 
   toolbox.dependencies = {
     exists,
+    existsDev,
     add,
   }
 }
