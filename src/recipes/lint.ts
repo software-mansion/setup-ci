3e53ece732259b0a6cad83b347d6eae4fa9e2621
km1chno <katzper.michno@gmail.com>
2024-07-23 18:06:43 +0200

    feat: create default eslint config file if one is missing

--- src/recipes/lint.ts
+++ src/recipes/lint.ts
@@ -1,15 +1,38 @@
 import { Toolbox } from 'gluegun/build/types/domain/toolbox'
 import { ProjectContext } from '../types'
+import { join } from 'path'
 
 const FLAG = 'lint'
 
+const ESLINT_CONFIGURATION_FILES = [
+  '.eslintrc.js',
+  '.eslintrc.json',
+  'eslint.config.js',
+  'eslint.config.mjs',
+  'eslint.config.cjs',
+]
+
+const existsEslintConfigurationFile = (toolbox: Toolbox): boolean =>
+  toolbox.filesystem
+    .list()
+    .filter((f) => ESLINT_CONFIGURATION_FILES.includes(f)).length > 0
+
 const execute = () => async (toolbox: Toolbox, context: ProjectContext) => {
-  await toolbox.dependencies.add('eslint', context.packageManager, true)
+  // eslint@9.x introduces new configuration format that is not supported by widely used plugins yet.
+  // https://eslint.org/docs/latest/use/migrate-to-9.0.0
+  await toolbox.dependencies.add('eslint@^8', context.packageManager, true)
 
   await toolbox.scripts.add('lint', 'eslint "**/*.{js,jsx,ts,tsx}"')
 
+  if (!existsEslintConfigurationFile(toolbox)) {
+    await toolbox.template.generate({
+      template: join('lint', '.eslintrc.ejs'),
+      target: '.eslintrc.js',
+    })
+  }
+
   await toolbox.workflows.generate(
-    'lint.ejf',
+    join('lint', 'lint.ejf'),
     context.path.absFromRepoRoot('.github', 'workflows', 'lint.yml'),
     context
   )
