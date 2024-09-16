33702ae81bb7a34eb27862eed4e817b63c3f1d88
km1chno <katzper.michno@gmail.com>
2024-09-16 15:17:42 +0200

    bump to dev version when publishing to react-native-ci-cli

---
+++ scripts/bump-version-dev.ts
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const { execSync } = require('child_process')
+const semver = require('semver')
+
+const packageJSON = require('../package.json')
+
+const currentVersion = packageJSON.version
+
+const timestamp = Math.floor(Date.now() / 1000)
+const newVersion = `${semver.inc(currentVersion, 'patch')}-dev.${timestamp}`
+
+packageJSON.version = newVersion
+fs.writeFileSync('package.json', JSON.stringify(packageJSON, null, 2))
+
+console.log(`Updated version to ${newVersion}`)
